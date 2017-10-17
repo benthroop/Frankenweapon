8cd9f561fc0f7e2677c4c584af256aad85a28971
Ben Throop <ben@frameinteractive.com>
2017-10-17 16:29:22 -0400

    Health and Damage

---
+++ Assets/Scripts/Damager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    public float damageAmount;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Health health = collision.gameObject.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TryTakeDamage(damageAmount);
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    public float currentHP;
+    public float maxHP;
+
+    public UnityEvent OnTakeDamage;
+    public UnityEvent OnDie;
+
+    public bool TryTakeDamage(float hp)
+    {
+        currentHP -= hp;
+        if (currentHP < 0f)
+        {
+            currentHP = 0f;
+            Die();
+        }
+
+        if (OnTakeDamage != null)
+        {
+            OnTakeDamage.Invoke();
+        }
+
+        return true;
+    }
+
+    public void Die()
+    {
+        if (OnDie != null)
+        {
+            OnDie.Invoke();
+        }
+    }
+	
+}

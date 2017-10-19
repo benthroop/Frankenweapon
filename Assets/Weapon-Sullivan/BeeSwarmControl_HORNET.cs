7d5ef9f69e3421f4245272160a50fe7cec9be6d5
Ben Throop <ben@frameinteractive.com>
2017-10-19 11:02:12 -0400

    Weapon-Sullivan first commit
    BEES! Worked first try, nice!

---
+++ Assets/Weapon-Sullivan/BeeSwarmControl_HORNET.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeSwarmControl_HORNET : MonoBehaviour {
+
+    public float speed;
+    float RotationSpeed = 3f; 
+    GameObject Target;
+    private Vector3 Difference;
+    private float atan2;
+    Rigidbody rb;
+    bool LockedOn = false;
+    float ColliderTick = 1f;
+    BoxCollider DetectionCollider;
+    float Attacks = 4f;
+    float LifeTick = 30f;
+    public GameObject OOOH;
+    bool IsDead = false;
+ 
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        DetectionCollider = GetComponent<BoxCollider>();  
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        ColliderTick -= Time.deltaTime;
+        LifeTick -= Time.deltaTime; 
+        if(ColliderTick <= 0)
+        {
+            DetectionCollider.enabled = true; 
+        }
+        if(LifeTick <= 0)
+        {
+            Destroy(gameObject); 
+        }
+
+        if (LockedOn == true)
+        {
+            Vector3 TargetDirection = Target.transform.position - transform.position;
+            float step = RotationSpeed * Time.deltaTime;
+            Vector3 NewDirection = Vector3.RotateTowards(transform.forward, TargetDirection, step, 0.0F);
+            transform.rotation = Quaternion.LookRotation(NewDirection);
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        if (IsDead == false)
+        {
+            rb.AddRelativeForce(Vector3.forward * speed);
+        }
+       
+    }
+
+    void OnCollisionEnter (Collision other)
+    {
+        rb.AddForce(-Vector3.forward * 2, ForceMode.Impulse);
+        if (other.gameObject.tag == "Player")
+        {
+            Attacks--;
+            CheckAttacks();
+            Instantiate(OOOH, gameObject.transform.position, gameObject.transform.rotation); 
+        }
+
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            LockedOn = true;
+            Target = other.gameObject; 
+        }
+    }
+
+    void CheckAttacks ()
+    {
+        if (Attacks == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Death()
+    {
+        rb.useGravity = true;
+        IsDead = true; 
+    }
+}
---
+++ Assets/Weapon-Sullivan/DAPS_HORNET.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DAPS_HORNET : Weapon
+{
+
+    public override void PrimaryFireStart()
+    {
+        isPrimaryFiring = true;
+        currentFiringTimer = nextPrimaryFireTime;
+    }
+
+    public override void PrimaryFireEnd()
+    {
+        isPrimaryFiring = false; 
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void FireBEES ()
+    {
+        MuzzleFlash.Emit(5); 
+        Instantiate(BeeSwarm, Barrel1.transform.position, Barrel1.transform.rotation);
+        Instantiate(BeeSwarm, Barrel2.transform.position, Barrel2.transform.rotation);
+        Instantiate(BeeSwarm, Barrel3.transform.position, Barrel3.transform.rotation);
+        Instantiate(BeeSwarm, Barrel4.transform.position, Barrel4.transform.rotation);
+        Instantiate(BeeSwarm, Barrel5.transform.position, Barrel5.transform.rotation);
+        Instantiate(BeeSwarm, Barrel6.transform.position, Barrel6.transform.rotation);
+    }
+	
+	
+
+    private bool isPrimaryFiring = false;
+    private float nextPrimaryFireTime = 0f;
+    private float currentFiringTimer = 0f;
+    public float primaryFireRepeatDelay;
+    public ParticleSystem MuzzleFlash; 
+    public GameObject BeeSwarm;
+    public GameObject Barrel1;
+    public GameObject Barrel2;
+    public GameObject Barrel3;
+    public GameObject Barrel4;
+    public GameObject Barrel5;
+    public GameObject Barrel6;
+
+    public void Update()
+    {
+        if (isPrimaryFiring)
+        {
+            if (currentFiringTimer >= nextPrimaryFireTime)
+            {
+                FireBEES();
+                nextPrimaryFireTime = primaryFireRepeatDelay;
+                currentFiringTimer = 0f;
+            }
+            else
+            {
+                currentFiringTimer += Time.deltaTime;
+            }
+        }
+    }
+}

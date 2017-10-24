308d93996ea0c399d586cbd5a0cf6f40bbb523cd
Ben Throop <ben@frameinteractive.com>
2017-10-24 00:14:26 -0400

    Tag players as "Player". Shared audio listener

---
+++ Assets/Scripts/SharedAudioListener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedAudioListener : MonoBehaviour 
+{
+	private List<GameObject> players;
+
+	void Start()
+	{
+		players = new List<GameObject>(GameObject.FindGameObjectsWithTag("Player"));
+	}
+
+	void LateUpdate()
+	{	
+		Vector3 pos = Vector3.zero;
+
+		for (int i=0; i<players.Count; i++)
+		{
+			pos += players[i].transform.position;
+		}
+
+		transform.position = Vector3.Scale(pos, new Vector3(1f/players.Count, 1f/players.Count, 1f/players.Count));
+		//TODO: Rotation
+	}
+
+}

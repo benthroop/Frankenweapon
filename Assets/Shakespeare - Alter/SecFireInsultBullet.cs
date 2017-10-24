d80106a3d65cdc1f9efd95f74ae6f48440c63b08
Dylan Alter <alterdylan@gmail.com>
2017-10-24 16:18:37 -0400

    Implemented Shakespeare Gun

---
+++ Assets/Shakespeare - Alter/SecFireInsultBullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecFireInsultBullet : MonoBehaviour {
+
+    public float lifetime = 4f;
+    public float TimeBeforeDeath;
+    public GameObject InsultVisual2;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer();
+        if (TimeBeforeDeath <= 1)
+        {
+            //Debug.Log("Reset");
+            Destroy(this);
+        }
+    }
+
+    void Timer()
+    {
+        TimeBeforeDeath -= Time.deltaTime;
+        // Debug.Log(TimeBeforeDeath);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
+
+        //Destroy(InsultVisual2);
+    }
+}

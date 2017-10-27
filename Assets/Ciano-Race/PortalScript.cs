2f1e95ec55c22166ce013e829523db9d4653f0ad
JaredCiano <33000747+JaredCiano@users.noreply.github.com>
2017-10-27 12:54:19 -0400

    Creation Of Portals
    I began work on portals that will teleport cars from one end to the other. This should allow us to make tracks that aren't simple looks.

---
+++ Assets/Ciano-Race/PortalScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour {
+
+	public GameObject TeleportSpot;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.GetComponent<PlayerVehicleController> () != null) {
+			
+			col.gameObject.transform.rotation = TeleportSpot.transform.rotation;
+			col.gameObject.transform.position = TeleportSpot.transform.position;
+
+		}
+	}
+}

8e5c131667ee3e10cbb10198fa76bcc8d83d5da6
LeonardGingello <leonard.gingello@mymail.champlain.edu>
2017-10-29 20:59:08 -0400

    2nd attempt at pushing for starting to work?

---
+++ Assets/Race-Gingello/VehicleGingello.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleGingello : VehicleBase 
+{
+	[SerializeField] WheelCollider frontRight;
+	[SerializeField] WheelCollider frontLeft;
+	[SerializeField] WheelCollider backRight;
+	[SerializeField] WheelCollider backLeft;
+
+	public float maxSteer;
+	public float maxTorque;
+
+	void Start () 
+	{
+		//this is to keep the wheels from jittering
+		frontRight.ConfigureVehicleSubsteps(5f, 12, 15);
+		frontLeft.ConfigureVehicleSubsteps(5f, 12, 15);
+		backRight.ConfigureVehicleSubsteps(5f, 12, 15);
+		backLeft.ConfigureVehicleSubsteps(5f, 12, 15);
+	}
+
+	void Drive()
+	{
+		//turning
+		frontLeft.steerAngle = steeringControlValue * maxSteer;
+		frontRight.steerAngle = steeringControlValue * maxSteer;
+
+		//torque
+		backLeft.motorTorque = throttleControlValue * maxTorque;
+		backRight.motorTorque = throttleControlValue * maxTorque;
+
+		//notice that the wheel visuals do NOT turn. You might want to make that work if it's visible to the player.
+		//there's actually a bit about that in the Unity Wheelcollider tutorial: https://docs.unity3d.com/Manual/WheelColliderTutorial.html
+	}
+	
+	void Update () 
+	{
+		Drive ();
+	}
+
+	public override void BoostStart()
+	{
+		//all you
+	}
+
+	public override void BoostStop()
+	{
+		//all you
+	}
+
+	public override void ActionStart()
+	{
+		//all you
+	}
+
+	public override void ActionStop()
+	{
+		//all you
+	}
+}

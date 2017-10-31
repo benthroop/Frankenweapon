272dabcb7a56fbde80da6a8ad619e205dd8d6613
Conor Tully <Conor.tully@mymail.champlain.edu>
2017-10-30 22:54:10 -0400

    Fix Rotate

--- Assets/Race-Tully/VehicleTully.cs
+++ Assets/Race-Tully/VehicleTully.cs
@@ -1,37 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
-public class VehicleTully : VehicleBase 
+public class VehicleTully: VehicleBase
 {
-	[SerializeField] WheelCollider frontRight;
-	[SerializeField] WheelCollider frontLeft;
-	[SerializeField] WheelCollider backRight;
-	[SerializeField] WheelCollider backLeft;
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backRight;
+    [SerializeField] WheelCollider backLeft;
 
-    [System.Serializable]
-    public class AxleInfo
+    public float maxSteer;
+    public float maxTorque;
+
+    void Start()
     {
-        public WheelCollider leftWheel;
-        public WheelCollider rightWheel;
-        public bool motor;
-        public bool steering;
+        //this is to keep the wheels from jittering
+        frontRight.ConfigureVehicleSubsteps(5f, 12, 15);
+        frontLeft.ConfigureVehicleSubsteps(5f, 12, 15);
+        backRight.ConfigureVehicleSubsteps(5f, 12, 15);
+        backLeft.ConfigureVehicleSubsteps(5f, 12, 15);
     }
 
-    public List<AxleInfo> axleInfos;
-    public float maxMotorTorque;
-    public float maxSteeringAngle;
-    public float maxSteer;
-	public float maxTorque;
+    void Drive()
+    {
+        //turning
+        frontLeft.steerAngle = steeringControlValue * maxSteer;
+        frontRight.steerAngle = steeringControlValue * maxSteer;
 
-	void Start () 
-	{
-		//this is to keep the wheels from jittering
-		frontRight.ConfigureVehicleSubsteps(5f, 12, 15);
-		frontLeft.ConfigureVehicleSubsteps(5f, 12, 15);
-		backRight.ConfigureVehicleSubsteps(5f, 12, 15);
-		backLeft.ConfigureVehicleSubsteps(5f, 12, 15);
-	}
+        //torque
+        backLeft.motorTorque = throttleControlValue * maxTorque;
+        backRight.motorTorque = throttleControlValue * maxTorque;
+
+        ApplyLocalPositionToVisuals(frontLeft);
+        ApplyLocalPositionToVisuals(frontRight);
+        ApplyLocalPositionToVisuals(backLeft);
+        ApplyLocalPositionToVisuals(backRight);
+
+
+    }
+
+    void FixedUpdate()
+    {
+        Drive();
+
+        if (transform.position.x < -29) transform.position = new Vector3(29, transform.position.y, transform.position.z);
+        if (transform.position.x > 29) transform.position = new Vector3(-29, transform.position.y, transform.position.z);
+        if (transform.position.z < -29) transform.position = new Vector3(transform.position.x, transform.position.y, 29);
+        if (transform.position.z > 29) transform.position = new Vector3(transform.position.x, transform.position.y, -29);
+    }
 
     public void ApplyLocalPositionToVisuals(WheelCollider collider)
     {
@@ -46,68 +61,30 @@ public class VehicleTully : VehicleBase
         Quaternion rotation;
         collider.GetWorldPose(out position, out rotation);
 
+
+
         visualWheel.transform.position = position;
         visualWheel.transform.rotation = rotation;
+
     }
 
-    void Drive()
-	{
-		//turning
-		frontLeft.steerAngle = steeringControlValue * maxSteer;
-		frontRight.steerAngle = steeringControlValue * maxSteer;
-
-		//torque
-		backLeft.motorTorque = throttleControlValue * maxTorque;
-		backRight.motorTorque = throttleControlValue * maxTorque;
-
-		//notice that the wheel visuals do NOT turn. You might want to make that work if it's visible to the player.
-		//there's actually a bit about that in the Unity Wheelcollider tutorial: https://docs.unity3d.com/Manual/WheelColliderTutorial.html
-	}
-
-    public void FixedUpdate()
+    public override void BoostStart()
     {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
-
-        foreach (AxleInfo axleInfo in axleInfos)
-        {
-            if (axleInfo.steering)
-            {
-                axleInfo.leftWheel.steerAngle = steering;
-                axleInfo.rightWheel.steerAngle = steering;
-            }
-            if (axleInfo.motor)
-            {
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
-            }
-            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
-            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
-        }
+        //all you
     }
 
-        void Update () 
-	{
-		Drive ();
-	}
+    public override void BoostStop()
+    {
+        //all you
+    }
 
-	public override void BoostStart()
-	{
-		//all you
-	}
+    public override void ActionStart()
+    {
+        //all you
+    }
 
-	public override void BoostStop()
-	{
-		//all you
-	}
-
-	public override void ActionStart()
-	{
-		//all you
-	}
-
-	public override void ActionStop()
-	{
-		//all you
-	}
+    public override void ActionStop()
+    {
+        //all you
+    }
 }

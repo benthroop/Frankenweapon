f0b5c206a1c2d1265d7864603b39f86918f128fe
Conor Tully <Conor.tully@mymail.champlain.edu>
2017-10-30 22:24:51 -0400

    Add (Broken) Wheel Turning

--- Assets/Race-Tully/VehicleTully.cs
+++ Assets/Race-Tully/VehicleTully.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class VehicleTully : VehicleBase 
 {
@@ -8,7 +9,19 @@ public class VehicleTully : VehicleBase
 	[SerializeField] WheelCollider backRight;
 	[SerializeField] WheelCollider backLeft;
 
-	public float maxSteer;
+    [System.Serializable]
+    public class AxleInfo
+    {
+        public WheelCollider leftWheel;
+        public WheelCollider rightWheel;
+        public bool motor;
+        public bool steering;
+    }
+
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+    public float maxSteer;
 	public float maxTorque;
 
 	void Start () 
@@ -20,7 +33,24 @@ public class VehicleTully : VehicleBase
 		backLeft.ConfigureVehicleSubsteps(5f, 12, 15);
 	}
 
-	void Drive()
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    void Drive()
 	{
 		//turning
 		frontLeft.steerAngle = steeringControlValue * maxSteer;
@@ -33,8 +63,30 @@ public class VehicleTully : VehicleBase
 		//notice that the wheel visuals do NOT turn. You might want to make that work if it's visible to the player.
 		//there's actually a bit about that in the Unity Wheelcollider tutorial: https://docs.unity3d.com/Manual/WheelColliderTutorial.html
 	}
-	
-	void Update () 
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+    }
+
+        void Update () 
 	{
 		Drive ();
 	}

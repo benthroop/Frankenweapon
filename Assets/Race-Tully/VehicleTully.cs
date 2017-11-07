3ef47c49464cc79b6fb0915b9cff52eaea21e6bf
Conor Tully <Conor.tully@mymail.champlain.edu>
2017-11-07 15:30:06 -0500

    Add Downforce to Car Add Color to Walls

--- Assets/Race-Tully/VehicleTully.cs
+++ Assets/Race-Tully/VehicleTully.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class VehicleTully: VehicleBase
 {
     [SerializeField] WheelCollider frontRight;
@@ -10,6 +11,9 @@ public class VehicleTully: VehicleBase
 
     public float maxSteer;
     public float maxTorque;
+    public float downforce;
+
+    private Rigidbody myRigidbody;
 
     void Start()
     {
@@ -18,6 +22,11 @@ public class VehicleTully: VehicleBase
         frontLeft.ConfigureVehicleSubsteps(5f, 12, 15);
         backRight.ConfigureVehicleSubsteps(5f, 12, 15);
         backLeft.ConfigureVehicleSubsteps(5f, 12, 15);
+        myRigidbody = GetComponent<Rigidbody>();
+        if (myRigidbody == null)
+        {
+            Debug.LogError("You a Stupid");
+        }
     }
 
     void Drive()
@@ -41,11 +50,38 @@ public class VehicleTully: VehicleBase
     void FixedUpdate()
     {
         Drive();
-
+        if (IsGrounded())
+        {
+            AddDownForce();
+        }
         if (transform.position.x < -29) transform.position = new Vector3(29, transform.position.y, transform.position.z);
         if (transform.position.x > 29) transform.position = new Vector3(-29, transform.position.y, transform.position.z);
         if (transform.position.z < -29) transform.position = new Vector3(transform.position.x, transform.position.y, 29);
         if (transform.position.z > 29) transform.position = new Vector3(transform.position.x, transform.position.y, -29);
+    }
+
+    public void AddDownForce()
+    {
+        myRigidbody.AddForce(-transform.up * downforce * myRigidbody.velocity.magnitude);
+    }
+
+    private bool IsGrounded()
+    {
+        WheelHit leftHit;
+        WheelHit rightHit;
+        bool result = false;
+        
+        if(frontLeft.GetGroundHit(out leftHit))
+        {
+            result = true;
+        }
+       if (frontRight.GetGroundHit(out rightHit))
+        {
+            result = true;
+        }
+
+        return result;
+
     }
 
     public void ApplyLocalPositionToVisuals(WheelCollider collider)
